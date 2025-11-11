728cec971c1da29ea5a9272e12ba6fa5b4169de0
rufus <rjc256@exeter.ac.uk>
2025-11-11 23:41:01 +0000

    more cleanup

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -5,57 +5,56 @@ import keras
 import keras_tuner
 import numpy as np
 import pandas as pd
-from keras import metrics
 from sklearn.model_selection import train_test_split
 
 
-def normalise(self, column, log=False, extra_divisor=1):
+def normalise(self, column, log=False, invert=False):
     if log:
-        self[column] = self[column].map(lambda value: 999 if value <= 0 else np.log10(value))
-    min_val = self[column].min()
-    max_val = self[column].max()
-    if max_val == np.inf:
-        max_val = 0
-    range_val = max_val - min_val
-    if range_val != 0:
-        self[column] = self[column].map(lambda value: value / (range_val * extra_divisor))
-    else:
-        self[column] = self[column].map(lambda value: value / extra_divisor)
+        self[column] = self[column].map(lambda value: 0 if value <= 0 else np.log10(value))
+    max_val = self[column].abs().max()
+    self[column] = self[column].map(lambda value: value / max_val)
+    if invert:
+        self[column] = self[column].map(lambda value: value * -1)
 
 
-# Oo oo aa aa monkey patch madness
 pd.DataFrame.normalise = normalise
-
-# Make numpy values easier to read.
-np.set_printoptions(precision=3, suppress=True)
 
 # Read in our csv file
 print("Reading .csv file...")
 data = pd.read_csv('datasets/outputs.csv')
 data = data.drop('ninc', axis=1)
+
+# Drop any inf values
+data.replace([np.inf, -np.inf], np.nan, inplace=True)
+data.dropna(inplace=True)
+
 # Split it into I/O form
 x, y = data.iloc[:, :14], data.iloc[:, 14:]
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+print("x:")
+print(x)
+print("y:")
+print(y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 # Normalise the training set
 print("Normalising dataset...")
-x_train.normalise("amin1", True, -1.2)
-x_train.normalise("amax1", True, 3)
+x_train.normalise("amin1", True, True)
+x_train.normalise("amax1", True)
 x_train.normalise("inclinations")
-x_train.normalise("Stellar_age", True, 3)
+x_train.normalise("Stellar_age", True)
 x_train.normalise("mass1")
-x_train.normalise("Temp_sublimation", extra_divisor=1599)
-x_train.normalise("router", extra_divisor=2)
+x_train.normalise("Temp_sublimation")
+x_train.normalise("router")
 x_train.normalise("height")
-x_train.normalise("betadisc", extra_divisor=3)
+x_train.normalise("betadisc")
 x_train.normalise("alphadisc")
 x_train.normalise("mdisc")
 x_train.normalise("Stellar_radius")
 x_train.normalise("Stellar_temperature")
 x_train.normalise("rinner")
 print("Normalising Y values...")
-for index, row in enumerate(y_train):
-    y_train.normalise(row, True, -1)
+for row in y_train:
+    y_train.normalise(row, True, True)
 
 print("Stitching DataFrames...")
 df = x_train.join(y_train)
@@ -64,16 +63,13 @@ df.to_csv('datasets/normalised.csv', index=False)
 # Create the validation set
 print("Creating validation set...")
 training_length = len(x_train)
-training_ratio = 0.8
-
+training_ratio = 0.2
 print(
-    f"Splitting {training_length} datapoints - training set gets {np.floor(training_length * training_ratio)},"
-    f" validation gets {np.floor(training_length * (1 - training_ratio))}")
+    f"Splitting {training_length} datapoints - validation set gets {np.floor(training_length * training_ratio)},"
+    f" training gets {np.floor(training_length * (1 - training_ratio))}")
 split_point = int(np.floor(training_length * training_ratio))
-x_val = x_train[-split_point:]
-y_val = y_train[-split_point:]
-x_train = x_train[:-split_point]
-y_train = y_train[:-split_point]
+x_val, y_val = x_train[-split_point:], y_train[-split_point:]
+x_train, y_train = x_train[:-split_point], y_train[:-split_point]
 
 # Normalisation test 2?
 print("Creating normalisation layer...")
@@ -88,22 +84,20 @@ def build_model(hp):
     model.add(normalize)
     model.add(keras.layers.Flatten())
     # Tune the number of layers.
-    for i in range(hp.Int("num_layers", 2, 6)):
+    for i in range(4):
         model.add(
             keras.layers.Dense(
                 # Tune number of units separately.
-                units=hp.Int(f"units_{i}", min_value=64, max_value=640, step=64),
-                activation=hp.Choice("activation", ["relu"]),
+                units=hp.Int(f"units_{i}", min_value=100, max_value=400, step=25),
+                activation="relu",
             )
         )
-    if hp.Boolean("dropout"):
-        model.add(keras.layers.Dropout(rate=0.25))
     model.add(keras.layers.Dense(100, activation="softmax"))
-    learning_rate = hp.Float("lr", min_value=1e-5, max_value=1e-3, sampling="log")
+    learning_rate = hp.Float("lr", min_value=1e-4, max_value=1e-2, sampling="log")
     model.compile(
         optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
-        loss="mse",
-        metrics=["accuracy"],
+        loss="mean_absolute_error",
+        metrics=["mse"],
     )
     return model
 
@@ -116,6 +110,4 @@ tuner = keras_tuner.RandomSearch(
     executions_per_trial=2,
 )
 
-tuner.search(x_train, y_train,
-             epochs=10,
-             validation_data=(x_val, y_val))
+tuner.search(x_train, y_train, epochs=10, validation_data=(x_val, y_val))
